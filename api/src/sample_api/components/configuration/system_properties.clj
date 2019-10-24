f6fd778f670dfda60a9189c3fd24251ce1ad6198
angelicogfa <angelicogfa@gmail.com>
2019-10-24 11:02:54 -0300

    inclusao

--- api/src/sample_api/components/configuration/system_properties.clj
+++ api/src/sample_api/components/configuration/system_properties.clj
@@ -1,10 +1,12 @@
 (ns sample-api.configuration.system-properties
   (:require [environ.core :refer [env]]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [sample-api.components.database.database :refer [new-database]]))
 
 (def settings {:environment :prod
                :host        "0.0.0.0"
                :port         8080
                :database {:connection-string (env :connection-string "jdbc:sqlserver://localhost:1433;databaseName=sample_database;user=sa;password=P@ssw0rd1!;")}})
+
 
 
